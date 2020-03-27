9dd7009d21b5b31032cec4d9578c3dbbe80b628d
Marya Doery <mdoery@gmail.com>
2020-03-26 21:08:29 -0400

    This app consists of a MainActivity which displays the WebView of a simple HTML page on the Internet. If the user does not interact with this page for a brief period of time, then a screensaver video replaces the view. This is the initial source for the app. MainActivity implements View.OnTouchListener in order to detect touches on the WebView. ScreenSaver is a simple Runnable that runs after a short timeout if no touches are detected. It takes you to VideoViewActivity with an Intent.

---
+++ app/src/main/java/com/fullstackoasis/webviewscreensaverontouch/MainActivity.java
@@ -0,0 +1,108 @@
+package com.fullstackoasis.webviewscreensaverontouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+    private String TAG = MainActivity.class.getCanonicalName();
+    private static String URL = "https://www.fullstackoasis.com/webview-screensaver-ontouch/";
+    private Handler waitHandler;
+    private StartScreenSaver startScreenSaver = new StartScreenSaver();
+    private static int TIME_TO_SCREENSAVER_SECONDS = 10;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Context applicationContext = this.getApplicationContext();
+        final WebView myWebView = new WebView(applicationContext);
+        setContentView(myWebView);
+        myWebView.setOnTouchListener(this);
+
+        // Always reset the user's cache when this activity is created.
+        clearCookies();
+
+        // Show the web page in a WebView
+        myWebView.loadUrl(URL);
+
+        // A screensaver video will open after a brief timeout, provided the user does not
+        // interact with the webview. User interaction is detected via onTouch.
+        startTimerToOpenScreenSaverAfterDelay(TIME_TO_SCREENSAVER_SECONDS);
+    }
+
+
+    /**
+     * When a user touches the view to interact with the website being shown in the WebView,
+     * the timer for showing a screensaver is reset.
+     * @param v
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        startTimerToOpenScreenSaverAfterDelay(TIME_TO_SCREENSAVER_SECONDS);
+        return false;
+    }
+
+    private void startTimerToOpenScreenSaverAfterDelay(int delayInSeconds) {
+        if (waitHandler != null) {
+            // See https://stackoverflow.com/questions/22718951/stop-handler-postdelayed/22719065
+            waitHandler.removeCallbacksAndMessages(null);
+        } else {
+            waitHandler = new Handler();
+        }
+        waitHandler.postDelayed(startScreenSaver, delayInSeconds*1000);
+    }
+
+    /**
+     * This method opens the ViewViewActivity, which shows a screensaver.
+     */
+    protected void goToVideoViewActivity() {
+        Intent i = new Intent(this, VideoViewActivity.class);
+        startActivity(i);
+    }
+
+    /**
+     * Remove any users cookies for privacy.
+     */
+    protected void clearCookies() {
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookies(null);
+    }
+
+    /**
+     * If the activity paused, that probably means the user is no longer interacting with the
+     * WebView, so remove cookies.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        clearCookies();
+    }
+
+    /**
+     * If the activity stopped, that probably means the user is no longer interacting with the
+     * WebView, so remove cookies.
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        clearCookies();
+    }
+
+    /**
+     * A Runnable that can be run to open the VideoViewActivity that shows a screensaver.
+     */
+    class StartScreenSaver implements Runnable {
+        @Override
+        public void run() {
+            goToVideoViewActivity();
+        }
+    }
+}
---
+++ app/src/main/java/com/fullstackoasis/webviewscreensaverontouch/VideoViewActivity.java
@@ -0,0 +1,38 @@
+package com.fullstackoasis.webviewscreensaverontouch;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VideoViewActivity extends AppCompatActivity implements View.OnTouchListener {
+    private static String TAG = VideoViewActivity.class.getCanonicalName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        VideoView view = (VideoView)findViewById(R.id.videoView);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.slomoflame_512kb;
+        view.setVideoURI(Uri.parse(path));
+        view.start();
+        view.setOnTouchListener(this);
+    }
+
+    /**
+     * Go back to the MainActivity.
+     * @param v unused
+     * @param event unused
+     * @return unused
+     */
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        return false;
+    }
+}
